3728993de31d7d7833630b2da1ed75b76dbdf13d
Henrique <heduardocr7@gmail.com>
2024-03-22 19:30:59 -0300

    style: Styling applied to the NavBar component and use of dark mode.

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <main className='flex justify-center items-center mx-auto mt-28'>
+        <section className='flex items-center flex-col'>
+            <h1 className="text-7xl font-semibold">Olá, Eu sou Henrique!</h1>
+            <h1 className="text-4xl font-medium mt-2 underline underline-offset-4 decoration-green-500"><span>{'Desenvolvedor Fullstack'}</span></h1>
+            <p className='mt-2'>Atualmente cursando Análise e Desenvolvimento de Sistemas - IFRN</p>
+        </section>
+
+        <section>
+
+        </section>
+    </main>
+  )
+}
